6459bc2d94b370454b0261e5b7a4b2e5a7384927
Nazmul Haque Rahat <nazmulrahat786.13@gmail.com>
2025-09-16 01:21:00 +0600

    Update useAxiosSecure.jsx

--- src/Hooks/AxiosSecure/useAxiosSecure.jsx
+++ src/Hooks/AxiosSecure/useAxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import useAuth from '../useAuth';
-
+ 
 const axiosSecure = axios.create({
     baseURL: 'https://building-management-system-server-a.vercel.app'
 })
